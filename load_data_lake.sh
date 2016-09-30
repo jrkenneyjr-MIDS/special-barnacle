7cf4417aaaf507cdbfdda93a55cfc11bf18ee796
John Kenney <jrkenneyjr@ischool.berkeley.edu>
2016-09-30 00:28:03 -0500

    Added HDFS Commands to Shell Script

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -1,9 +1,12 @@
-unzip Hospital_Revised_Flatfiles.zip -d Hospital
-cd Hospital
+unzip hospital_compare_Revised_Flatfiles.zip -d hospital_compare
+cd hospital_compare
 mkdir Finished
 for f in *.csv; do tail -n +2 "$f" > Finished/"$f"; done
 
 mv Finished ~/w205-fall-16-labs-exercises/special-barnacle
 cd ~/w205-fall-16-labs-exercises/special-barnacle
-rm -R Hospital
-mv Finished Hospital
+rm -R hospital_compare
+mv Finished hospital_compare
+
+hdfs dfs -put hospital_compare /user/w205/
+hdfs dfs -du /user/w205/hospital_compare
