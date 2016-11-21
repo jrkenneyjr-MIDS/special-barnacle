084c281e9fbafd80e0dbe11eaade7030d1a1c198
John Kenney <root@ip-172-31-1-127.ec2.internal>
2016-11-21 06:59:25 +0000

    Updates

--- tweetwordcount/finalresults.py
+++ tweetwordcount/finalresults.py
@@ -6,14 +6,17 @@ cur = conn.cursor()
 
 #Select
 if len(sys.argv) == 1:
- 	cur.execute("SELECT * FROM Tweetwordcount WHERE word !~ '^[1-9]' ORDER BY word ASC LIMIT 10;")
+ 	cur.execute("SELECT * FROM Tweetwordcount WHERE word !~ '^[1-9]' ORDER BY word ASC LIMIT 50;")
 	records = cur.fetchall()
 	for rec in records:
    		print ("word = " + str(rec[0]) + '\t' + "count = " + str(rec[1]))
 else:
-	cur.execute("SELECT word, count from Tweetwordcount where word = %s;",(sys.argv[1],))
-  	rec = cur.fetchone()
-	print("Total occurrences of \"" + str(rec[0]) + "\": " + str(rec[1]))
+	try:
+		cur.execute("SELECT word, count from Tweetwordcount where word = %s;",(sys.argv[1],))
+  		rec = cur.fetchone()
+		print("Total occurrences of \"" + str(rec[0]) + "\": " + str(rec[1]))
+	except TypeError:
+		print("No instances found.")
 	
 conn.commit()
 
